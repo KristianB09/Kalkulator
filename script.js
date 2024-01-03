d438892581eb1fc959d42c3577a1c4ed9d03c6f0
kristianb09 <kristianb09.kodehode@gmail.com>
2024-01-03 12:58:14 +0100

    Started JS logic and added some styling to output.

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 class Calculator {
-  constructor() {}
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+    this.clear();
+  }
 
-  clear() {}
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operation = undefined;
+  }
 
   delete() {}
 
-  appendNumber(number) {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-  chooseOperation(operation) {}
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
   compute() {}
 
-  updateDisplay() {}
+  updateOutput() {
+    this.currentOperandText.innerText = this.currentOperand;
+    this.previousOperandText.innerText = this.previousOperand;
+  }
 }
 
 const numberButtons = document.querySelectorAll("[data-number]");
@@ -19,9 +38,21 @@ const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const clearButton = document.querySelector("[data-all-clear]");
 const deleteButton = document.querySelector("[data-delete]");
-const previousOperandTextElement = document.querySelector(
-  "[data-previous-operand]"
-);
-const currentOperandTextElement = document.querySelector(
-  "[data-current-operand]"
-);
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateOutput();
+  });
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateOutput();
+  });
+});
